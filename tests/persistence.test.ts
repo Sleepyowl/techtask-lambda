79340623eb1270ed4d1dd92187952dda3061191d
Dmitry Soloviev <dmitry.soloviev@outlook.com>
2026-05-20 10:25:09 +0200

    Add fake aws api key for tests

--- tests/persistence.test.ts
+++ tests/persistence.test.ts
@@ -2,6 +2,8 @@ const TABLE_NAME = "NotificationsTest";
 process.env.NOTIFICATIONS_TABLE = TABLE_NAME;
 process.env.DYNAMODB_ENDPOINT = "http://localhost:8000";
 process.env.AWS_REGION = "local";
+process.env.AWS_ACCESS_KEY_ID = "local";
+process.env.AWS_SECRET_ACCESS_KEY = "local";
 
 import { randomUUID } from "crypto";
 
